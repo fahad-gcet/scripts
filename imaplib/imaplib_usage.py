b412a3666567cc80acaa19ea32762a75b3d126c6
fahad-gcet <fms525@outlook.com>
2017-08-21 18:01:43 +0530

    add retreive email and save attachements

---
+++ imaplib/imaplib_usage.py
@@ -0,0 +1,67 @@
+import configparser
+import imaplib
+import email
+import re
+import os
+
+def connect():
+	config = configparser.ConfigParser()
+	config.read([os.path.expanduser('~/.fahadConfig')])
+
+	hostname = config.get('server', 'hostname')
+	port = config.get('server', 'port')
+
+	m = imaplib.IMAP4_SSL(hostname, port)
+
+	username = config.get('account', 'username')
+	password = config.get('account', 'password')
+
+	m.login(username, password)
+	m.select()
+
+	return m
+
+
+# Using PEEK so that we don't change the UNREAD status of the email
+def getMailByID(m, emailid):
+	resp, data = m.fetch(emailid, "(BODY.PEEK[])")
+	email_body = data[0][1].decode('utf-8')
+	mail = email.message_from_string(email_body)
+	return mail
+
+
+def getMailDetail(m, emailid):
+	mail = getMailByID(m, emailid)
+	print('FROM: ', mail['from'])
+	print('SUBJECT: ', mail['subject'])
+	for part in mail.walk():
+		if part.get_content_type() == 'text/html':
+			html = part.get_payload(decode=True)
+	html = html.decode("utf-8")
+	html = re.sub('<[^<]+?>', '', html)
+	html = html.strip()
+	print('***START BODY ***')
+	print(html)
+	print('***END BODY ***')
+
+def downloaAttachmentsInEmail(m, emailid, outputdir):
+	mail = getMailByID(m, emailid)
+	if mail.get_content_maintype() != 'multipart':
+		return
+	for part in mail.walk():
+		if part.get_content_maintype() != 'multipart' and part.get('Content-Disposition') is not None:
+			open(outputdir + '/' + part.get_filename(), 'wb').write(part.get_payload(decode=True))
+
+
+def retreiveEmails(m, outputdir):
+	resp, items = m.search(None, '(FROM "fms525@outlook.com" SUBJECT "Final Test Mail")')
+	items = items[0].split()
+	for emailid in items:
+		getMailDetail(m, emailid)
+		downloaAttachmentsInEmail(m, emailid, outputdir)
+
+
+if __name__ == '__main__':
+	m = connect()
+	retreiveEmails(m, '.')
+
