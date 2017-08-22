782bfff17d34fc2fbd415b1765b8122be27fcbb7
fahad-gcet <fms525@outlook.com>
2017-08-22 17:22:20 +0530

    add db and read text attachments

--- imaplib/imaplib_usage.py
+++ imaplib/imaplib_usage.py
@@ -3,6 +3,13 @@ import imaplib
 import email
 import re
 import os
+import pymysql
+from dateutil.parser import parse
+
+
+def db_connect():
+	db = pymysql.connect("localhost","root","mindfire","test_db")
+	return db
 
 
 def connect():
@@ -33,8 +40,6 @@ def getMailByID(m, emailid):
 
 def getMailDetail(m, emailid):
 	mail = getMailByID(m, emailid)
-	print('FROM: ', mail['from'])
-	print('SUBJECT: ', mail['subject'])
 	for part in mail.walk():
 		if part.get_content_type() == 'text/html':
 			html = part.get_payload(decode=True)
@@ -42,29 +47,63 @@ def getMailDetail(m, emailid):
 	# Remove HTML tags
 	html = re.sub('<[^<]+?>', '', html)
 	# Remove spaces
-	html = html.strip()
-	print('***START BODY ***')
-	print(html)
-	print('***END BODY ***')
+	body = html.strip()
+	
+	return mail['from'], mail['to'] ,mail['subject'], body, mail['date']
 
-def downloaAttachmentsInEmail(m, emailid, outputdir):
+
+def downloaAttachmentsInEmail(m, emailid, outputdir, mail_id):
 	mail = getMailByID(m, emailid)
 	if mail.get_content_maintype() != 'multipart':
 		return
 	for part in mail.walk():
 		if part.get_content_maintype() != 'multipart' and part.get('Content-Disposition') is not None:
-			open(outputdir + '/' + part.get_filename(), 'wb').write(part.get_payload(decode=True))
+			file_path = outputdir + '/' + part.get_filename()
+			open(file_path, 'wb').write(part.get_payload(decode=True))
+			F = open(file_path,'r')
+
+
+			db = db_connect()
+			cursor = db.cursor()
+			sql = "INSERT INTO attachments(mail_id, attachment_path, parsed_data) VALUES ('%s', '%s', '%s')" % (mail_id, os.path.realpath(F.name), F.read())
+
+			try:
+				cursor.execute(sql)
+				db.commit()
+			except Exception as e:
+				print(e)
+				db.rollback()
+			db.close()
+			
+		
 
 
 def retreiveEmails(m, outputdir):
 	resp, items = m.search(None, '(FROM "fms525@outlook.com" SUBJECT "Final Test Mail")')
 	items = items[0].split()
 	for emailid in items:
-		getMailDetail(m, emailid)
-		downloaAttachmentsInEmail(m, emailid, outputdir)
+		sender, receiver, subject, body, created_at =  getMailDetail(m, emailid)
+		print('FROM: ', sender)
+		print('TO: ', receiver)
+		print('SUBJECT: ', subject)
+		dt = parse(created_at)
+		dt = dt.strftime('%Y-%m-%d %H:%M:%S')
+		print('CREATED AT: ', dt)
+		db = db_connect()
+		cursor = db.cursor()
+		sql = "INSERT INTO emails(sender, receiver, subject, body, created_at) VALUES ('%s', '%s', '%s', '%s', '%s' )" % (sender, receiver, subject, body, dt)
+		try:
+			cursor.execute(sql)
+			db.commit()
+			mail_id = cursor.lastrowid
+		except Exception as e:
+			print(e)
+			db.rollback()
+		db.close()
+		downloaAttachmentsInEmail(m, emailid, outputdir, mail_id)
 
 
 if __name__ == '__main__':
 	m = connect()
-	retreiveEmails(m, '.')
+	retreiveEmails(m, './downloads')
 
